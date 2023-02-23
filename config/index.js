3c641031b1b5aa8310d62c987409708e3481d989
da001416 <126132904+da001416@users.noreply.github.com>
2023-02-23 11:27:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wx8bd32863b3e75a42",
+  appsecret: "eb0d4fbb492869e0942de6435131285e",
+  template_id: "EfVRS9rzvt7PwfpaS119NsOFP_HrNBzxSSobfaBTKuo",
+  touser: "oD_yO57dSpgQau4hqIkonV_lnvUA",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "宿迁",
+  first_date: "2020-11-28",
 };
